1e1c3e124cebf65d0af97b7be33142385cf64eeb
0906796 <0906796@hr.nl>
2018-06-25 10:00:52 +0200

    Minor fixes

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Middleware;
 
+use App\User;
 use Closure;
 use Illuminate\Support\Facades\Auth;
 
@@ -10,15 +11,23 @@ class RedirectIfAuthenticated
     /**
      * Handle an incoming request.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure  $next
-     * @param  string|null  $guard
+     * @param  \Illuminate\Http\Request $request
+     * @param  \Closure $next
+     * @param  string|null $guard
      * @return mixed
      */
     public function handle($request, Closure $next, $guard = null)
     {
+
         if (Auth::guard($guard)->check()) {
-            return redirect('/home');
+            $user = User::where('email', $request->email)->first();
+            if ($user->hasRole('admin')) {
+                return redirect()->route('home.admin');
+            } else if ($user->hasRole('operator')) {
+                return redirect()->route('home.operator');
+            } else if ($user->hasRole('client')) {
+                return redirect()->route('home.client');
+            }
         }
 
         return $next($request);
